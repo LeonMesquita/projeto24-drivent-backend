2a0a2ded0fb5e5cb1421e72b026cc25cd4acc1b5
LeonMesquita <leonmesquita16@gmail.com>
2022-12-24 18:28:37 -0300

    test: Signin tests

---
+++ tests/factories/auth-factory.ts
@@ -0,0 +1,16 @@
+import supertest from 'supertest'
+import app from '../../src/index'
+import { AuthBody, AuthData } from '../../src/interfaces/authInterface'
+
+export async function signupFactory (): Promise<AuthData> {
+  const body: AuthBody = {
+    email: 'any_email@mail.com',
+    password: 'any_password',
+    confirmPassword: 'any_password'
+  }
+  await supertest(app).post('/sign-up').send(body)
+  return ({
+    email: body.email,
+    password: body.password
+  })
+}
